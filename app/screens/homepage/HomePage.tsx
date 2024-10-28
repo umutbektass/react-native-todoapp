f2cf0e7b747800ff3a6bd0c7306d9421468cde4b
Umut Bektaş <umutbektass.78@hotmail.com>
2024-10-28 17:54:50 +0300

    Refactor: Replace ScrollView with FlatList for CourseGoals list

--- app/screens/homepage/HomePage.tsx
+++ app/screens/homepage/HomePage.tsx
@@ -1,55 +1,75 @@
 import {
   Pressable,
-  ScrollView,
   Text,
   TextInput,
   View,
+  FlatList,
 } from "react-native";
 import React, { useState } from "react";
 import styles from "./Style";
 
+type CourseGoal = {
+  id: number;
+  text: string;
+};
+
 const HomePage = () => {
   const [enteredGoalText, setEnteredGoalText] = useState<string>("");
-  const [courseGoals, setCourseGoal] = useState<string[]>([]);
+  const [courseGoals, setCourseGoal] = useState<CourseGoal[]>([]);
   const goalInputHandler = (text: string) => {
     setEnteredGoalText(text);
   };
   const addGoalHandler = () => {
-    setCourseGoal((currentCourseGoals) => [
-      ...currentCourseGoals,
-      enteredGoalText,
-    ]);
+    setCourseGoal((currentCourseGoals) => {
+      const newId: number =
+        currentCourseGoals.length > 0
+          ? currentCourseGoals[courseGoals.length - 1].id + 1
+          : 1;
+      return [...currentCourseGoals, { id: newId, text: enteredGoalText }];
+    });
+
     setEnteredGoalText("");
   };
-  return (
-      <View style={styles.container}>
-        <View style={styles.textInputContainer}>
-          <TextInput
-            style={styles.textInput}
-            placeholder="Your course goal!"
-            value={enteredGoalText}
-            onChangeText={goalInputHandler}
-          />
-          <Pressable style={({pressed})=> [{backgroundColor: pressed ? '#7AB2D3'  :  '#4A628A'  },styles.button]} onPress={addGoalHandler}>
-            <Text style={styles.buttonText}> ADD GOAL </Text>
-          </Pressable>
-        </View>
-        <View style={styles.goalsContainer}>
-
-        <ScrollView >
-          {courseGoals.length > 0 ? (
-            courseGoals.map((item) => (
-              <View style={styles.goalItem} key={item}>
-                <Text style={styles.goalText}>{item}</Text>
-              </View>
-            ))
-          ) : (
-            <Text>List of goals...</Text>
-          )}
-        </ScrollView>
-        </View>
-
+  const renderItem = ({ item }: any) => {
+    return (
+      <View style={styles.goalItem} key={item.id}>
+        <Text style={styles.goalText}>{item.text}</Text>
       </View>
+    );
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          value={enteredGoalText}
+          onChangeText={goalInputHandler}
+        />
+        <Pressable
+          style={({ pressed }) => [
+            { backgroundColor: pressed ? "#7AB2D3" : "#4A628A" },
+            styles.button,
+          ]}
+          onPress={addGoalHandler}
+        >
+          <Text style={styles.buttonText}> ADD GOAL </Text>
+        </Pressable>
+      </View>
+      <View style={styles.goalsContainer}>
+        <FlatList
+         data={courseGoals}
+          renderItem={({item})=>{
+            return (
+              <View style={styles.goalItem} key={item.id}>
+              <Text style={styles.goalText}>{item.text}</Text>
+            </View>
+            )
+          }}
+          keyExtractor={item=>item.id.toString()}
+          />
+      </View>
+    </View>
   );
 };
 
